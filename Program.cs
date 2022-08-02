3cebf2eb321b51ec7013c51f627c3e747ff74c3f
filipe.bpinto <filipe.bpinto@OSAW062523610-1>
2022-08-01 21:18:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Numero_Chamada = 0;
+            String  Nome_Aluno = "Filipe Bastos";
+
+            Numero_Chamada = 1;
+            Console.WriteLine(Numero_Chamada);
+            Console.WriteLine(Nome_Aluno);
+
+            Console.WriteLine(args[0].ToString());
+            Console.WriteLine(args[1].ToString());
+            Console.WriteLine(args[2].ToString());
+            Console.WriteLine(args[3].ToString());
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
